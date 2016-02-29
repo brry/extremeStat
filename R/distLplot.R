33367b45c870470039092f3511e4adaa22b71e4e
brry <berryboessenkool@hotmail.com>
2016-02-29 15:36:52 +0100

    distLplot: can now draw quantile lines, also handles dlf elements more cleanly

--- R/distLplot.R
+++ R/distLplot.R
@@ -11,7 +11,7 @@
 #' @seealso \code{\link{distLfit}}, \code{\link{distLextreme}}
 #' @keywords hplot distribution
 #' @export
-#' @importFrom berryFunctions lim0 owa seqPal
+#' @importFrom berryFunctions lim0 owa rainbow2
 #' @importFrom lmomco plmomco dlmomco supdist
 #' @examples
 #'  # See distLfit
@@ -33,13 +33,20 @@
 #' @param col \code{\link{hist}} bar color or \code{\link{ecdf}} point color. DEFAULT: "grey"
 #' @param main,xlab,ylab \code{\link{hist}} or \code{\link{ecdf}} main, xlab, ylab. DEFAULT: internal abstraction fom \code{dlf$datname}
 #' @param las Label Axis Style for orientation of numbers along axes. DEFAULT: 1
-#' @param coldist Color for each distribution added with \code{\link{lines}}. DEFAULT: \code{\link[berryFunctions]{seqPal}}
+#' @param coldist Color for each distribution added with \code{\link{lines}}. DEFAULT: \code{\link[berryFunctions]{rainbow2}}
 #' @param lty Line TYpe for plotted distributions. Recycled vector of length nbest. DEFAULT: 1
 #' @param add If TRUE, hist is not called before adding lines. This lets you add lines highly customized one by one. DEFAULT: FALSE
 #' @param logargs List of arguments passed to \code{\link{logAxis}} if \code{log=TRUE}. DEFAULT: NULL
 #' @param legend Should \code{\link{legend}} be called? DEFAULT: TRUE
 #' @param legargs List of arguments passed to \code{\link{legend}} except for legend and col. DEFAULT: NULL
 #' @param histargs List of arguments passed to \code{\link{hist}} or \code{\link{ecdf}} except for x, freq. DEFAULT: NULL
+#' @param qlines Should vertical lines marking the quantiles be added?
+#'        Ignored if \code{dlf} does not contain the element \code{quant}. DEFAULT: FALSE
+#' @param qheights Coordinates of quantile line ends, recycled if necessary. DEFAULT: 20\% of plot height.
+#' @param qrow Rowname(s) of \code{dlf$quant} that should be drawn instead of the nbest highest ranking distribution functions.
+#'             'q_gpd*' will select all the gpd fits, including gpa.
+#'             qheights and coldist must then accordingly have at least 13 elements (or will be recycled). DEFAULT: NULL
+#' @param qlinargs Arguments passed to \code{\link{lines}} for qlines. DEFAULT: NULL
 #' @param \dots Further arguments passed to \code{\link{lines}}, like type, pch, ...
 #' 
 distLplot <- function(
@@ -53,41 +60,41 @@ percentline=NA,
 percentargs=NULL,
 supportends=TRUE,
 breaks=20,
-xlim=extendrange(dat, f=0.15),
+xlim=extendrange(dlf$dat, f=0.15),
 ylim=NULL,
 xaxs="i", yaxs="i",
 xaxt,
 col="grey",
 main, xlab, ylab,
 las=1,
-coldist=berryFunctions::seqPal(nbest),
+coldist=berryFunctions::rainbow2(nbest),
 lty=1,
 add=FALSE,
 logargs=NULL,
 legend=TRUE,
 legargs=NULL,
 histargs=NULL,
+qlines=FALSE,
+qheights=stats::quantile(par("usr")[3:4], 0.2),
+qrow=NULL,
+qlinargs=NULL,
 ... )
 {
 # input checks:
 if(!is.list(dlf)) stop("dlf must be a list.")
-# Objects from list:
-dat       <- dlf$dat
-parameter <- dlf$parameter
-gof       <- dlf$gof
 # checking list elements:
-if(is.null(dat)) stop("dlf must contain the element dat")
-if(is.null(parameter)) stop("dlf must contain the element parameter")
-if(is.null(gof)) stop("dlf must contain the element gof")
+if(is.null(dlf$dat)) stop("dlf must contain the element dat")
+if(is.null(dlf$parameter)) stop("dlf must contain the element parameter")
+if(is.null(dlf$gof)) stop("dlf must contain the element gof")
 if(is.null(dlf$datname)) stop("dlf must contain the element datname")
 if(is.null(dlf$gofProp)) stop("dlf must contain the element gofProp")
 # distribution selection:
 if(!is.null(selection))
   {
   names(selection) <- selection
-  if(order) selection <- selection[rownames(gof)]
+  if(order) selection <- selection[rownames(dlf$gof)]
   selection <- selection[!is.na(selection)]
-  sing <- selection %in% rownames(gof)
+  sing <- selection %in% rownames(dlf$gof)
   if(!any(sing)) stop("selection (", pastec(selection[!sing]), ") is not available in dlf$gof.")
   if(any(!sing)) 
     {
@@ -95,23 +102,34 @@ if(!is.null(selection))
     on.exit(message("Note in disLplot: selection (", curselsing, ") is not available in dlf$gof."), add=TRUE)
     }
   selection <- selection[sing]
-  gof <- gof[selection, ]
+  dlf$gof <- dlf$gof[selection, ]      # continuing with a different variant of gof!!
   nbest <- length(selection)
   }
 # input checks:
 if(nbest < 0) stop("nbest must be positive")
-if(nbest > nrow(gof)) {nbest <- nrow(gof)}
+if(nbest > nrow(dlf$gof) & is.null(qrow)) {nbest <- nrow(dlf$gof)}
 # internal defaults:
 if(missing(xaxt)       ) xaxt <- if(log) "n" else "s"
 if(missing(xlab)       ) xlab <- dlf$datname
-# draw histogram or ecdf
+#
+if(!is.null(qrow))
+  {
+  if(any(qrow=="q_gpd*")) qrow <- c(qrow[qrow!="q_gpd*"], "gpa", "q_gpd_evir_pwm",
+         "q_gpd_evir_ml", "q_gpd_evd", "q_gpd_extRemes_MLE", "q_gpd_extRemes_GMLE",
+         "q_gpd_extRemes_Bayesian", "q_gpd_extRemes_Lmoments", "q_gpd_fExtremes_pwm",
+         "q_gpd_fExtremes_mle", "q_gpd_ismev", "q_gpd_Renext_r", "q_gpd_Renext_f")
+  dnqrow <- unique(qrow)
+  }
+#
+# draw histogram or ecdf -------------------------------------------------------
 if(!add)
+{
   if(cdf)
   {
   if(is.null(ylim)) ylim <- c(0,1)
   if(missing(ylab)) ylab <- "(Empirical) Cumulated Density (CDF)"
   if(missing(main)) main <- paste("Cumulated density distributions of", dlf$datname)
-  ecdfdef <- list(x=ecdf(dat), do.points=TRUE, col=col, xlim=xlim, xaxt=xaxt, ylab=ylab,
+  ecdfdef <- list(x=ecdf(dlf$dat_full), do.points=TRUE, col=col, xlim=xlim, xaxt=xaxt, ylab=ylab,
              ylim=ylim, xaxs=xaxs, yaxs=yaxs, main=main, xlab=xlab, las=las)
   do.call(plot, args=berryFunctions::owa(ecdfdef, histargs, "x", "y"))
   if(log)
@@ -121,12 +139,12 @@ if(!add)
   }
   else # if not cdf, then density
   {
-  if(is.null(ylim)) ylim <- berryFunctions::lim0(hist(dat, breaks=breaks, plot=FALSE)$density,
+  if(is.null(ylim)) ylim <- berryFunctions::lim0(hist(dlf$dat, breaks=breaks, plot=FALSE)$density,
                                   curtail=if(yaxs=="i") FALSE else TRUE)
   if(missing(ylab)) ylab <- "Probability Density Function (PDF)"
   if(missing(main)) main <- paste("Density distributions of", dlf$datname)
   op <- par(xaxs=xaxs, yaxs=yaxs, xaxt=xaxt)
-  histdef <- list(x=dat, breaks=breaks, col=col, xlim=xlim, ylim=ylim, ylab=ylab,
+  histdef <- list(x=dlf$dat, breaks=breaks, col=col, xlim=xlim, ylim=ylim, ylab=ylab,
                   freq=FALSE, main=main, xlab=xlab, las=las)
   do.call(hist, args=berryFunctions::owa(histdef, histargs, "x", "freq"))
   if(log)
@@ -135,24 +153,27 @@ if(!add)
     }
   par(op)
   }
-# distribution names and colors:
+}
+#
+# distribution names and colors: -----------------------------------------------
 if(nbest < 1) return(invisible(NULL)) # and stop executing
-dn <- rownames(gof)[1:nbest]
-coldist <- rep(coldist, length=nbest)
-if(length(parameter)<1)
+dn <- rownames(dlf$gof)[1:nbest]
+if(qlines) if(!is.null(qrow)) dn <- dnqrow[dnqrow %in% rownames(dlf$gof)]
+coldist <- rep(coldist, length=nbest)  # recycle 1
+if(length(dlf$parameter)<1)
   {
   message("Note in distLplot: no distributions were available in dlf$parameter.")
   dlf$coldist <- coldist
   return(invisible(dlf))
   }
 lty <- rep(lty, length=nbest)
-# add distributions:
+# add distribution function lines:
 if(cdf) lfun <- lmomco::plmomco else lfun <- lmomco::dlmomco
-for(i in nbest:1)
+for(i in length(dn):1)
   {
   xval <- seq(from=par("usr")[1], to=par("usr")[2], length=300)
   # cut xval to support region
-  paramd <- parameter[[dn[i]]]
+  paramd <- dlf$parameter[[dn[i]]]
   xsup <- lmomco::supdist(paramd)$support
   xval <- xval[ xval>xsup[1] & xval<xsup[2] ]
   # last point within support range, if support ends in graphing region:
@@ -165,12 +186,23 @@ for(i in nbest:1)
   }
 # draw vertical gofProp line:
 if(is.na(percentline)) percentline <- if(dlf$gofProp!=1) TRUE else FALSE
-if(percentline) do.call(abline, args=berryFunctions::owa(list(v=quantile(dat, probs=1-dlf$gofProp),
+if(percentline) do.call(abline, args=berryFunctions::owa(list(v=quantile(dlf$dat, probs=1-dlf$gofProp),
                                                 lty=3, col="red"), percentargs))
+# draw quantile lines:
+if(qlines)
+  {
+  if(is.null(dlf$quant)) stop("qlines=TRUE, but dlf does not contain 'quant' element.")
+  if(!is.null(qrow)) dn <- dnqrow
+  # qheights <- lfun(x=qval, para=dlf$parameter[[use[i]]])
+  qheights <- rep(qheights, len=length(dn))
+  coldist <- rep(coldist, len=length(dn)) # recycle 2
+  for(i in seq_len(ncol(dlf$quant))) do.call(graphics::lines, args=berryFunctions::owa(list(
+      x=dlf$quant[dn,i], y=qheights, type="h", col=coldist), qlinargs, "x","y"))
+  }
 # legend - write the names of distributions:
 legdef <- list(legend=dn, lwd=1, col=coldist, x="right", cex=0.7, lty=lty)
 if(legend) do.call(graphics::legend, args=berryFunctions::owa(legdef, legargs, "legend","col","lty"))
-# add to output:
+# add to (or change) output:
 dlf$coldist <- coldist
 return(invisible(dlf))
 } # end function
