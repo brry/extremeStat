24ad9188992a58a31bfb3218c5f91be8f89f5e21
brry <berryboessenkool@hotmail.com>
2016-06-08 17:01:14 +0200

    lap and gld added back in (lmomco bugs fixed)

--- R/distLplot.R
+++ R/distLplot.R
@@ -177,8 +177,8 @@ for(i in length(dn):1)
   #browser()
   if(is.null(paramd)) next # 
   if(!is.null(paramd$ifail)) if(paramd$ifail!=0) next # emu # this may be too strict...
-  if(paramd$type=="gld") next # lmomco 2.2.2 cdfgld bug
-  if(!is.null(paramd$error)) if(paramd$convergence!=0) next # gep 
+  ### if(paramd$type=="gld") next # lmomco 2.2.2 cdfgld bug
+  if(!is.null(paramd$error)) if(!is.null(paramd$convergence)) if(paramd$convergence!=0) next # gep 
   xsup <- lmomco::supdist(paramd)$support
   xval <- xval[ xval>xsup[1] & xval<xsup[2] ]
   # only plot distribution line if there is some support:
